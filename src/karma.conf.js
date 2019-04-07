1ee1d4b936c9b59fc3b2bde946638020e7037d82
Victoremepunto <victoremepunto@gmail.com>
2019-04-07 22:17:41 +0200

    Added cobertura reporter

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -17,7 +17,7 @@ module.exports = function (config) {
     },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, '../coverage/angular-unit-testing'),
-      reports: ['html', 'lcovonly', 'text-summary'],
+      reports: ['html', 'lcovonly', 'text-summary','cobertura'],
       fixWebpackSourcePaths: true
     },
     reporters: ['progress', 'kjhtml'],
